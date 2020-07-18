ae242b41a7899b433f39b2706d896230682d521f
Tharindu <htharindum1988@gmail.com>
2020-07-18 07:28:18 +0530

    Initial commit

---
+++ src/AppInitializer.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.applet.Applet;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(this.getClass().getResource("view/Mainform.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
